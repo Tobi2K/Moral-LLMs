ed4262b9a24445f97960a251cb96f6d8343825f3
Tobias Kalmbach <t.kalmbach35@gmail.com>
2023-11-11 08:41:59 +0100

    fix: Correct length

--- test-prompt.py
+++ test-prompt.py
@@ -20,7 +20,7 @@ sequences = pipeline(
     top_k=10,
     num_return_sequences=1,
     eos_token_id=tokenizer.eos_token_id,
-    max_length=200,
+    max_length=800,
 )
 for seq in sequences:
     print(f"Result: {seq['generated_text']}")
