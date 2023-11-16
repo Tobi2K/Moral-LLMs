585f009837a530f9a5c925f00e4d181a51d8cfaf
Tobias Kalmbach <t.kalmbach35@gmail.com>
2023-11-16 14:34:48 +0100

    Add rerun option and add more models

--- prompt-machine.py
+++ prompt-machine.py
@@ -5,35 +5,46 @@ import datetime
 import json
 
 
-def run_prompt_on_model(model_name, prompt, prompt_title=""):
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    pipeline = transformers.pipeline(
-        "text-generation",
-        model=model_name,
-        torch_dtype=torch.float16,
-        device_map="auto",
-    )
-    sequences = pipeline(
-        prompt,
-        do_sample=True,
-        top_k=10,
-        num_return_sequences=1,
-        eos_token_id=tokenizer.eos_token_id,
-        max_length=800,
-    )
-    cur_date = str(datetime.datetime.now()).replace(
-        ' ', '--').replace(':', '').replace('.', '')
-    with open(model.replace('/', '') + cur_date + prompt_title + '.txt', 'w') as f:
-        for seq in sequences:
-            print(f"Result: {seq['generated_text']}")
-            f.write(f"Model: {model_name}\n\n")
-            f.write(f"Date: {datetime.datetime.now()}\n\n")
-            f.write(f"Prompt: {prompt}\n\n")
-            f.write(f"Result: {seq['generated_text']}\n")
+def run_prompt_on_model(model_name, prompt, prompt_title="", with_context=False, reruns=1):
+    # if with_context is set, do not reset model between repetitions
+    if with_context:
+        runs_with_context = reruns
+        runs_with_reset = 1
+    # else create a new model for every prompt
+    else:
+        runs_with_context = 1
+        runs_with_reset = reruns
+    
+    for _ in range(runs_with_reset):
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        pipeline = transformers.pipeline(
+            "text-generation",
+            model=model_name,
+            torch_dtype=torch.float16,
+            device_map="auto",
+        )
+        sequences = pipeline(
+            prompt,
+            do_sample=True,
+            top_k=10,
+            num_return_sequences=1,
+            eos_token_id=tokenizer.eos_token_id,
+            max_length=800,
+        )
+        for _ in range(runs_with_context):
+            cur_date = str(datetime.datetime.now()).replace(
+                ' ', '--').replace(':', '').replace('.', '')
+            with open(model.replace('/', '') + cur_date + prompt_title + '.txt', 'w') as f:
+                for seq in sequences:
+                    print(f"Result: {seq['generated_text']}")
+                    f.write(f"Model: {model_name}\n\n")
+                    f.write(f"Date: {datetime.datetime.now()}\n\n")
+                    f.write(f"Prompt: {prompt}\n\n")
+                    f.write(f"Result: {seq['generated_text']}\n")
 
 
 if __name__ == "__main__":
-    models = ["meta-llama/Llama-2-7b-chat-hf"]
+    models = ["meta-llama/Llama-2-7b-hf", "meta-llama/Llama-2-7b-chat-hf", "meta-llama/Llama-2-13b-hf", "meta-llama/Llama-2-13b-chat-hf", "georgesung/llama2_7b_chat_uncensored", "Tap-M/Luna-AI-Llama2-Uncensored"]
 
     prompt_title = "SDC-Man-FemaleExecutive-VS-Homeless-Homeless"
 
