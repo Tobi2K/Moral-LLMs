f61f257b0ac53b4e5cc11808c00815e55aa024e3
Tobias Kalmbach <t.kalmbach35@gmail.com>
2023-12-16 16:35:07 +0100

    Change ordering

--- LLMQuestionnaire/questionnaire.py
+++ LLMQuestionnaire/questionnaire.py
@@ -200,7 +200,7 @@ if __name__ == "__main__":
         "Tap-M/Luna-AI-Llama2-Uncensored",
     ]
 
-    ordering = ["St", "Q", "Sc"]
+    ordering = ["St", "Sc", "Q"]
     # Go through all combinations
     for parameters in itertools.product([True, False], repeat=5):
         prompt, prompt_title = generate_prompt(
